b1cba0301e6e043de76183cd316606fda56229b8
Tonie Victor <victornwanochi@gmail.com>
2023-12-19 06:17:57 -0800

    feat: impelmented the quick sort algo using the hoare partition scheme.

---
+++ 107-quick_sort_hoare.c
@@ -0,0 +1,75 @@
+#include "sort.h"
+
+/**
+ * quick_sort_hoare - a function that sorts an array of integers
+ * in ascending order using the Quick sort algorithm.
+ * This function uses the hoare partition scheme.
+ *
+ * @array: the array to be sorted.
+ * @size: size of the array to be sorted.
+ */
+void quick_sort_hoare(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+
+	hoare_recur(array, 0, size - 1, size);
+}
+
+/**
+ * recursort - recursive implementation of the quick sort.
+ *
+ * @array: The array to be sorted.
+ * @low: the lowest index of the array.
+ * @high: The highest index of the array.
+ * @size: the size of the array (used mostly to print the array after swap).
+ */
+void hoare_recur(int *array, int low, int high, size_t size)
+{
+	int pivot;
+	if (low >= 0 && high >= 0 && low < high)
+	{
+		pivot = hoare_parti(array, low, high, size);
+		hoare_recur(array, low, pivot, size);
+		hoare_recur(array, pivot + 1, high, size);
+	}
+}
+
+/**
+ * hoare_parti - a functions that partitions an array int 2 sub arrayS
+ * using the hoare partition scheme.
+ *
+ * @array: The array to be sorted.
+ * @low: the lowest index of the array.
+ * @high: The highest index of the array.
+ * @size: the size of the array (used mostly to print the array after swap).
+ *
+ * Return: the index of the pivot after partitioning.
+ */
+
+int hoare_parti(int *array, int low, int high, size_t size)
+{
+	int pivot = array[((high - low) / 2) + low];
+	int i = low - 1;
+	int j = high + 1;
+	int temp;
+
+	while (1)
+	{
+		do {
+			i = i + 1;
+		} while (array[i] < pivot);
+
+		do {
+			j = j - 1;
+		} while (array[j] > pivot);
+
+		if (i >= j)
+			return (j);
+
+		temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+		print_array(array, size);
+	}
+}
--- sort.h
+++ sort.h
@@ -29,5 +29,8 @@ void swap(int *x, int *y);
 void recursort(int *array, int low, int high, size_t size);
 int partition(int *array, int low, int high, size_t size);
 void shell_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void hoare_recur(int *array, int low, int high, size_t size);
+int hoare_parti(int *array, int low, int high, size_t size);
 
 #endif /* SORT_H */
